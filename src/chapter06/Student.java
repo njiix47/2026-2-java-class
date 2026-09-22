deef2b8726f7c1171fc87a3f56bc86a4171da210
njiix47 <nddkw@dgsw.hs.kr>
2026-09-23 08:28:56 +0900

    9/22(화) 객체지향 프로그래밍

--- src/chapter06/Student.java
+++ src/chapter06/Student.java
@@ -1,5 +1,7 @@
 package chapter06;
 
+import java.sql.SQLOutput;
+
 public class Student {
     int studentID;
     String studentName;
@@ -8,5 +10,30 @@ public class Student {
 
     public void showStudentInfo() {
         System.out.println(studentName + " " + address);
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String name) {
+        studentName = name;
+    }
+
+    public static void main(String[] args) {
+        Student studentLee = new Student();
+        Student studentKim = new Student();
+
+        studentLee.studentName = "이유찬";
+        studentKim.studentName = "김섭규";
+
+        System.out.println(studentLee.studentName);
+        System.out.println(studentLee.getStudentName());
+
+        System.out.println(studentKim.studentName);
+        System.out.println(studentKim.getStudentName());
+
+        System.out.println(studentLee);
+        System.out.println(studentKim);
     }
 }
